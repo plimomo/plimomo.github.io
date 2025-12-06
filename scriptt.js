8e45a6ac462b2a39e49f829c8210aac49c0786ac
plimomo <jirapatplum@gmail.com>
2025-12-06 16:29:00 +0700

    add all

--- scriptt.js
+++ scriptt.js
@@ -1,9 +1,9 @@
 let imgEe = document.getElementById("wife");
 
-imgEl.addEventListener("mouseover", () => {
+imgEe.addEventListener("mouseover", () => {
     imgEe.src = "/dvmjrvla3qj71.webp"
 })
 
-imgEl.addEventListener("mouseout", () => {
+imgEe.addEventListener("mouseout", () => {
     imgEe.src = "/Hu-Tao-PNG-Isolated-Image.png"
 })
