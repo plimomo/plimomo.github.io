b713de9d738db48b80d74a8e4e373a15af7ca418
plimomo <jirapatplum@gmail.com>
2025-12-06 16:22:09 +0700

    all

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello')
+let imgEl = document.getElementById("menuImg");
+
+imgEl.addEventListener("mouseover", () => {
+    imgEl.src = "/Iggy_PG.png"
+})
+
+imgEl.addEventListener("mouseout", () => {
+    imgEl.src = "/dddd.png"
+})
+
+
---
+++ scriptt.js
@@ -0,0 +1,9 @@
+let imgEe = document.getElementById("wife");
+
+imgEl.addEventListener("mouseover", () => {
+    imgEe.src = "/dvmjrvla3qj71.webp"
+})
+
+imgEl.addEventListener("mouseout", () => {
+    imgEe.src = "/Hu-Tao-PNG-Isolated-Image.png"
+})
